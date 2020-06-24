89f853752de80d6d6921666bfe03bb9126c9247b
mdubon1407 <mdubonrs@yahoo.com>
2020-06-23 18:54:48 -0600

    Add files via upload

---
+++ Quiz2/js.js
@@ -0,0 +1,87 @@
+function Quiz(questions) {
+  this.score = 0;
+  this.questions = questions;
+  this.currentQuestionIndex = 0;
+}
+
+Quiz.prototype.guess = function(answer) {
+  if(this.getCurrentQuestion().isCorrectAnswer(answer)) {
+      this.score++;
+  }
+  this.currentQuestionIndex++;
+};
+
+Quiz.prototype.getCurrentQuestion = function() {
+  return this.questions[this.currentQuestionIndex];
+};
+
+Quiz.prototype.hasEnded = function() {
+  return this.currentQuestionIndex >= this.questions.length;
+};
+function Question(text, choices, answer) {
+  this.text = text;
+  this.choices = choices;
+  this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function (choice) {
+  return this.answer === choice;
+};
+var QuizUI = {
+  displayNext: function () {
+      if (quiz.hasEnded()) {
+          this.displayScore();
+      } else {
+          this.displayQuestion();
+          this.displayChoices();
+          this.displayProgress();
+      }
+  },
+  displayQuestion: function() {
+      this.populateIdWithHTML("question", quiz.getCurrentQuestion().text);
+  },
+  displayChoices: function() {
+      var choices = quiz.getCurrentQuestion().choices;
+
+      for(var i = 0; i < choices.length; i++) {
+          this.populateIdWithHTML("choice" + i, choices[i]);
+          this.guessHandler("guess" + i, choices[i]);
+      }
+  },
+  displayScore: function() {
+      var gameOverHTML = "<h1>Game Over</h1>";
+      gameOverHTML += "<h2> Your score is: " + quiz.score + "</h2>";
+      this.populateIdWithHTML("quiz", gameOverHTML);
+  },
+  
+  populateIdWithHTML: function(id, text) {
+      var element = document.getElementById(id);
+      element.innerHTML = text;
+  },
+  guessHandler: function(id, guess) {
+      var button = document.getElementById(id);
+      button.onclick = function() {
+          quiz.guess(guess);
+          QuizUI.displayNext();
+      }
+  },
+  
+  displayProgress: function() {
+      var currentQuestionNumber = quiz.currentQuestionIndex + 1;
+      this.populateIdWithHTML("progress", "Question " + currentQuestionNumber + " of " + quiz.questions.length);
+  }
+};
+//Create Questions
+var questions = [
+  new Question("In what year was the first video game created?", [ "1955", "1961", "1981", "1979" ], "1961"),
+  new Question("Which one of these is javascript package manager?", ["node.js","typeScript", "npm", "I don't know"], "npm"),
+  new Question("Who invented JavaScript?", ["Brendan Eich","Douglas Crockford", "Sheryl Sandberg", "Andy Sandberg"], "Brendan Eich"),
+  new Question("Which tool can you use to ensure code quality?", ["RequireJS","Angular", "jQuery", "ESLint"], "ESLint"),
+  new Question("When a user views a page containing a JavaScript program, which machine actually excecutes the script?", ["The User's machine running a Web browser","The Web server", "A central machine deep within Netscape's corporate offices", "None of the above"], "The User's machine running a Web browser")
+];
+
+//Create Quiz
+var quiz = new Quiz(questions);
+
+//Display Quiz
+QuizUI.displayNext();
